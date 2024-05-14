afe4ccfee2220072f6f9723804a88fee89011d53
camargo-rubcube <guilherme.camargo@rubcube.com>
2024-05-14 11:59:09 -0300

    feat: initial screens and install tailwind

---
+++ src/views/AdminDashboardView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <div class="p-5 text-2xl text-white bg-blue-600">
+      Painel de Controle - Admin
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-5">
+      <!-- Cartão para Usuários -->
+      <div class="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition">
+        <h2 class="text-xl font-bold mb-2">Usuários</h2>
+        <p class="text-gray-700">Gerenciar todos os usuários do sistema.</p>
+        <button
+          class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Acessar
+        </button>
+      </div>
+
+      <!-- Cartão para Denúncias -->
+      <div class="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition">
+        <h2 class="text-xl font-bold mb-2">Denúncias</h2>
+        <p class="text-gray-700">Visualizar e gerenciar as denúncias.</p>
+        <button
+          class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Acessar
+        </button>
+      </div>
+
+      <!-- Cartão para Feedbacks -->
+      <div class="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition">
+        <h2 class="text-xl font-bold mb-2">Feedbacks</h2>
+        <p class="text-gray-700">Revisar o feedback dos cidadãos.</p>
+        <button
+          class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Acessar
+        </button>
+      </div>
+
+      <!-- Adicione mais cartões conforme necessário -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdminDashboardView",
+};
+</script>
+
+<style scoped>
+/* Adicionalmente, você pode adicionar estilos customizados aqui se necessário */
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-blue-300"
+  >
+    <div class="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
+      <h1 class="text-center text-2xl font-bold text-gray-700">
+        Bem-vindo ao Ativo e Operante!
+      </h1>
+      <form @submit.prevent="login" class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm font-medium text-gray-700"
+            >E-mail</label
+          >
+          <input
+            type="email"
+            id="email"
+            v-model="credentials.email"
+            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Digite seu e-mail"
+            required
+          />
+        </div>
+        <div>
+          <label for="password" class="block text-sm font-medium text-gray-700"
+            >Senha</label
+          >
+          <input
+            type="password"
+            id="password"
+            v-model="credentials.password"
+            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Digite sua senha"
+            required
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          >
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "LoginView",
+  setup() {
+    const router = useRouter();
+    const credentials = ref({ email: "", password: "" });
+    const error = ref("");
+
+    const login = async () => {
+      router.push("/admin/dashboard");
+    };
+
+    return { credentials, error, login };
+  },
+});
+</script>
